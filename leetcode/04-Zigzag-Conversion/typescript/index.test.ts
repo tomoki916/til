36e646632a410d7fb7d085d2a60242c9697f4728
深町知貴 <tomoki@coco-2.local>
2024-04-26 00:45:05 +0900

    add another problem

---
+++ leetcode/04-Zigzag-Conversion/typescript/index.test.ts
@@ -0,0 +1,22 @@
+import { main } from "."
+
+test("test case 1", () => {
+  const s = "PAYPALISHIRING"
+  const numRows = 3
+  const result = "PAHNAPLSIIGYIR"
+  expect(main(s, numRows)).toEqual(result)
+})
+
+test("test case 2", () => {
+  const s = "PAYPALISHIRING"
+  const numRows = 4
+  const result = "PINALSIGYAHRPI"
+  expect(main(s, numRows)).toEqual(result)
+})
+
+test("test case 3", () => {
+  const s = "A"
+  const numRows = 1
+  const result = "A"
+  expect(main(s, numRows)).toEqual(result)
+})
