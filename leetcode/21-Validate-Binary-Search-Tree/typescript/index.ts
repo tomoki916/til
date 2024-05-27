ccea201eb0e3b7c016617db5585608a739d1cb3f
深町知貴 <tomoki@coco-2.local>
2024-05-27 20:49:42 +0900

    add problem

--- leetcode/21-Validate-Binary-Search-Tree/typescript/index.ts
+++ leetcode/21-Validate-Binary-Search-Tree/typescript/index.ts
@@ -1,31 +1,45 @@
-function isValidBST(root: TreeNode | null): boolean {
+// function isValidBST(root: TreeNode | null): boolean {
+//   if (!root) return true;
+
+//   const isValid = (
+//     rootValue: number,
+//     node: TreeNode,
+//     side: "right" | "left"
+//   ): boolean => {
+//     if (node.left !== null && node.left.val >= node.val) return false;
+//     if (node.right !== null && node.right.val <= node.val) return false;
+//     if (side === "left" && node.val >= rootValue) return false;
+//     if (side === "right" && node.val <= rootValue) return false;
+
+//     const isValidLeft =
+//       node.left !== null ? isValid(rootValue, node.left, side) : true;
+//     const isValidRight =
+//       node.right !== null ? isValid(rootValue, node.right, side) : true;
+
+//     return isValidLeft && isValidRight;
+//   };
+
+//   const isValidLeft =
+//     root.left !== null
+//       ? isValid(root.val, root.left, "left") && isValidBST(root.left)
+//       : true;
+//   const isValidRight =
+//     root.right !== null
+//       ? isValid(root.val, root.right, "right") && isValidBST(root.right)
+//       : true;
+//   return isValidLeft && isValidRight;
+// }
+function isValidBST(
+  root: TreeNode | null,
+  min = -Infinity,
+  max = Infinity
+): boolean {
   if (!root) return true;
 
-  const isValid = (
-    rootValue: number,
-    node: TreeNode,
-    side: "right" | "left"
-  ): boolean => {
-    if (node.left !== null && node.left.val >= node.val) return false;
-    if (node.right !== null && node.right.val <= node.val) return false;
-    if (side === "left" && node.val >= rootValue) return false;
-    if (side === "right" && node.val <= rootValue) return false;
+  if (max <= root.val || min >= root.val) return false;
 
-    const isValidLeft =
-      node.left !== null ? isValid(rootValue, node.left, side) : true;
-    const isValidRight =
-      node.right !== null ? isValid(rootValue, node.right, side) : true;
-
-    return isValidLeft && isValidRight;
-  };
-
-  const isValidLeft =
-    root.left !== null
-      ? isValid(root.val, root.left, "left") && isValidBST(root.left)
-      : true;
-  const isValidRight =
-    root.right !== null
-      ? isValid(root.val, root.right, "right") && isValidBST(root.right)
-      : true;
-  return isValidLeft && isValidRight;
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  );
 }
