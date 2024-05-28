8b3fdf30e5dbfb3758c4d1aff0f5d0244ee10697
深町知貴 <tomoki@coco-2.local>
2024-05-28 22:42:04 +0900

    add problem

---
+++ leetcode/27-Minimum-Depth-of-Binary-Tree/typescript/index.ts
@@ -0,0 +1,9 @@
+function minDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+
+  if (!root.left && !root.right) return 1;
+
+  if (!root.left) return minDepth(root.right) + 1;
+  if (!root.right) return minDepth(root.left) + 1;
+  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+}
